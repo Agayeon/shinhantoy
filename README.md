60097934e117ff0e6740001f9bb5bf9e2d79a6f6
Agayeon <agy125@naver.com>
2023-01-26 18:23:10 +0900

    feat) create order detail page

--- README.md
+++
@@ -1,1 +0,0 @@
-# shinhantoy
--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, mixins
 
-from .serializers import OrderSerializer
-from .models import Order
+from .serializers import OrderSerializer, CommentSerializer, CommentCreateSerializer
+from .models import Order, Comment
 
 # Create your views here.
 
@@ -17,3 +17,42 @@ class OrderListView(
 
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = OrderSerializer
+    def get_queryset(self):
+        return Order.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, args, kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, args, kwargs)
+
+class CommentListView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    generics.GenericAPIView
+):
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CommentCreateSerializer
+        return CommentSerializer
+
+    def get_queryset(self):
+        order_id = self.kwargs.get('order_id')
+        return Comment.objects.filter(order_id=order_id)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
